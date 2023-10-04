b8a3e6af26c6f2aed59914c9483f93243c261fde
n01648028 <146011951+n01648028@users.noreply.github.com>
2023-10-04 11:08:14 -0400

    Added CAD to USD, USD to CAD, USD to USD, EUR to CAD, EUR to USD, GBP to CAD, GBP to USD, JPY to CAD, JPY to USD, CNY to CAD, CNY to USD, CHF to CAD, CHF to USD, INR to CAD, INR to USD
    Added CAD to USD, USD to CAD, USD to USD, EUR to CAD, EUR to USD, GBP to CAD, GBP to USD, JPY to CAD, JPY to USD, CNY to CAD, CNY to USD, CHF to CAD, CHF to USD, INR to CAD, INR to USD

--- Exchange.py
+++ Exchange.py
@@ -1,6 +1,13 @@
 if __name__ == '__main__':
     crossRateTable = {
-        "CAD": {"CAD": 1}
+        "CAD": {"CAD": 1, "USD": 0.7625},
+        "USD": {"CAD": 1.3118, "USD": 1},
+        "EUR": {"CAD": 1.4983, "USD": 1.1417},
+        "GBP": {"CAD": 1.6832, "USD": 1.2829},
+        "JPY": {"CAD": 0.0117, "USD": 0.0089},
+        "CNY": {"CAD": 0.1890, "USD": 0.1440},
+        "CHF": {"CAD": 1.3160, "USD": 1.0029},
+        "INR": {"CAD": 0.0179, "USD": 0.0137}
     }
 
     inHandCurrency = input("please enter the currency in hand (from): ")
