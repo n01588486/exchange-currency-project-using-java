bfe5d9d8a850ac063e0d7974a2c74e92570ef879
n01648028 <146011951+n01648028@users.noreply.github.com>
2023-10-04 10:41:48 -0400

    Added CAD to CAD currency conversion
    Added CAD to CAD currency conversion

--- Exchange.py
+++ Exchange.py
@@ -1,11 +1,15 @@
 if __name__ == '__main__':
-    inHandCurrency = input("please enter the currency in hand: ")
+    crossRateTable = {
+        "CAD": {"CAD": 1}
+    }
 
-    requiredCurrency = input("please enter the currency required: ")
+    inHandCurrency = input("please enter the currency in hand (from): ")
 
-    totalAmount = float(input("please enter amount: "))
+    requiredCurrency = input("please enter the currency required (to): ")
 
-    exchangeRate = float(input("please enter exchange rate:"))
+    totalAmount = float(input("please enter amount (current currency value): "))
+
+    exchangeRate = crossRateTable[inHandCurrency][requiredCurrency]
 
     exchangeAmount = totalAmount * exchangeRate
     print("Total exchange amount: " + str(exchangeAmount))
